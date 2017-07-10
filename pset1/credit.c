15852189f609d2561128d0762b0f25e81d36ea0e
Candice Davidson <cndragn@gmail.com>
2017-07-09 20:59:16 -0400

    Create credit.c

---
+++ pset1/credit.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+
+
+{
+    long long n;
+    
+    do 
+    {
+        printf("Number: ");
+        n = get_long_long();
+    }
+    while ( n < 0 ); 
+    
+    
+}
