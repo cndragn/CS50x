012c575a7064f862afb7b674c1148b0c683f1883
Candice Davidson <cndragn@gmail.com>
2017-07-11 23:38:49 -0400

    Replace with correct generate.c

--- pset3/find/generate.c
+++ pset3/find/generate.c
@@ -1,63 +1,52 @@
 /**
- * Prompts user for as many as MAX values until EOF is reached, 
- * then proceeds to search that "haystack" of values for given needle.
+ * generate.c
  *
- * Usage: ./find needle
+ * Generates pseudorandom numbers in [0,MAX), one per line.
  *
- * where needle is the value to find in a haystack of values
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
  */
-       
+ 
+#define _XOPEN_SOURCE
+
 #include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#include "helpers.h"
-
-// maximum amount of hay
-const int MAX = 65536;
+// upper limit on range of integers that can be generated
+#define LIMIT 65536
 
 int main(int argc, string argv[])
 {
-    // ensure proper usage
-    if (argc != 2)
+    // TODO: comment me
+    if (argc != 2 && argc != 3)
     {
-        printf("Usage: ./find needle\n");
-        return -1;
+        printf("Usage: ./generate n [s]\n");
+        return 1;
     }
 
-    // remember needle
-    int needle = atoi(argv[1]);
+    // TODO: comment me
+    int n = atoi(argv[1]);
 
-    // fill haystack
-    int size;
-    int haystack[MAX];
-    for (size = 0; size < MAX; size++)
+    // TODO: comment me
+    if (argc == 3)
     {
-        // wait for hay until EOF
-        printf("\nhaystack[%i] = ", size);
-        int straw = get_int();
-        if (straw == INT_MAX)
-        {
-            break;
-        }
-     
-        // add hay to stack
-        haystack[size] = straw;
-    }
-    printf("\n");
-
-    // sort the haystack
-    sort(haystack, size);
-
-    // try to find needle in haystack
-    if (search(needle, haystack, size))
-    {
-        printf("\nFound needle in haystack!\n\n");
-        return 0;
+        srand48((long) atoi(argv[2]));
     }
     else
     {
-        printf("\nDidn't find needle in haystack.\n\n");
-        return 1;
+        srand48((long) time(NULL));
     }
+
+    // TODO: comment me
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
 }
