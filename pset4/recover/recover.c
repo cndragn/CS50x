5768edbb42754625657e1ffad232606b19fe9450
Candice Davidson <cndragn@gmail.com>
2017-07-18 19:30:25 -0400

    Update through read 512 bytes into buffer

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,4 +1,25 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//data type
+typedef uint8_t  BYTE;
+
+//JFIFHEAD
+typedef struct _JFIFHeader
+{
+  BYTE SOI[2];          /* 00h  Start of Image Marker     */
+  BYTE APP0[2];         /* 02h  Application Use Marker    */
+  BYTE Length[2];       /* 04h  Length of APP0 Field      */
+  BYTE Identifier[5];   /* 06h  "JFIF" (zero terminated) Id String */
+  BYTE Version[2];      /* 07h  JFIF Format Revision      */
+  BYTE Units;           /* 09h  Units used for Resolution */
+  BYTE Xdensity[2];     /* 0Ah  Horizontal Resolution     */
+  BYTE Ydensity[2];     /* 0Ch  Vertical Resolution       */
+  BYTE XThumbnail;      /* 0Eh  Horizontal Pixel Count    */
+  BYTE YThumbnail;      /* 0Fh  Vertical Pixel Count      */
+} JFIFHEAD;
 
 int main(int argc, char *argv[]) 
 {
@@ -9,7 +30,7 @@ int main(int argc, char *argv[])
         return 1;
     }
     
-    //remember filename to recover
+    //filename to recover
     char *infile = argv[1];
     
     //open input file
@@ -17,7 +38,21 @@ int main(int argc, char *argv[])
     
     if(inptr == NULL)
     {
-        fprintf(stderr, "Could note open %s. \n", infile);
+        fprintf(stderr, "Could not open %s. \n", infile);
         return 2;
     }
+    
+    
+    //for length of card, read 512 bytes into a buffer
+    JFIFHEAD buffer;
+    while(fread(&buffer, 1, 512, inptr) != 0)
+    {
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[3] == 0xff && (buffer[3] && 0xf0) == 0xe0) {
+        
+        printf("Found it");
+        }
+    }
+    
+   return 1;
 }
+
