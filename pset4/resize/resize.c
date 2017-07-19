6c73f5568b05faf9f019e30cf3de6fb9fafae48b
Candice Davidson <cndragn@gmail.com>
2017-07-18 20:41:41 -0400

    Resize horizontally

--- pset4/resize/resize.c
+++ pset4/resize/resize.c
@@ -50,10 +50,18 @@ int main(int argc, char *argv[])
     // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+    
+    //BITMAPFILEHEADER for resize
+    BITMAPFILEHEADER newBf;
+    newBf = bf;
 
     // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+    
+    //BITMAPINFOHEADER for resize
+    BITMAPINFOHEADER newBi;
+    newBi = bi;
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 || 
@@ -64,15 +72,31 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Unsupported file format.\n");
         return 4;
     }
+    
+    //height and width times n value to enlarge by
+   newBi.biWidth = bi.biWidth * n;
+   newBi.biHeight = bi.biHeight * n;
+   
+   // determine padding for scanlines
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    
+    //padding for new height / width
+    int newPadding = (4 - (newBi.biWidth * sizeof(RGBTRIPLE)) % 4) %4;
+   
+  //new file size 
+   newBf.bfSize = 54 + (newBi.biWidth * sizeof(RGBTRIPLE) + newPadding) * abs(newBi.biHeight);
+   newBi.biSizeImage = newBf.bfSize - 54;
+    
 
     // write outfile's BITMAPFILEHEADER
-    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
+    fwrite(&newBf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
-    fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
+    fwrite(&newBi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
-    // determine padding for scanlines
-    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    
+    
+    
 
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
